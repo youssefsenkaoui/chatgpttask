37c00b0ae95505c83ab7755228e25589887f87c3
youssefsenkaoui <youssef.senkaoui29@gmail.com>
2024-03-11 17:19:04 +0000

    v4

--- analysis.php
+++ analysis.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 
 $excelFilePath = './fileExel/CHATGPT.xlsx';
 
@@ -21,26 +22,81 @@ $json = json_encode($data, JSON_PRETTY_PRINT);
 
 echo 'JSON Data: ' . $json . PHP_EOL;
 
+$jsonData = json_encode($json, JSON_PRETTY_PRINT);
+
+$filePath = './jsonfile/data.json';
+
+file_put_contents($filePath, $jsonData);
+
+if (file_exists($filePath)) {
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo 'JSON file created successfully at ' . $filePath;
+} else {
+    echo 'Error creating JSON file.';
+}
+
 $openaiApiKey = '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';
-$openaiEndpoint = 'https://api.openai.com/v1/files';
 
-$openaiData = [
-    'prompt' => $json,
-    'max_tokens' => 150,
-];
 
-$headers = [
-    'Content-Type' => 'application/json',
-    'Authorization' => 'Bearer ' . $openaiApiKey,
-];
+
+$jsonFilePath = './jsonfile/data.json';
+
+$jsonData = file_get_contents($jsonFilePath);
 
 $client = new Client();
-$response = $client->post($openaiEndpoint, [
-    'headers' => $headers,
-    'json' => $openaiData,
-]);
 
-$result = json_decode($response->getBody(), true);
-$chatGptAnalysis = $result['choices'][0]['text'];
+try {
+    $response = $client->post('https://api.openai.com/v1/files', [
+        'headers' => [
+            'Authorization' => 'Bearer ' . $openaiApiKey,
+        ],
+        'multipart' => [
+            [
+                'name' => 'file',
+                'contents' => $jsonData,
+                'filename' => 'data.json',
+            ],
+        ],
+    ]);
 
-echo 'ChatGPT Analysis: ' . $chatGptAnalysis . PHP_EOL;
+    $body = $response->getBody()->getContents();
+    echo $body;
+} catch (RequestException $e) {
+    echo 'Error: ' . $e->getMessage();
+    if ($e->hasResponse()) {
+        echo 'Response: ' . $e->getResponse()->getBody()->getContents();
+    }
+}
+
+
+
+
+
+
+
+
+
+// $openaiEndpoint = 'https://api.openai.com/v1/files';
+
+// $openaiData = [
+//     'prompt' => $json,
+//     'max_tokens' => 150,
+// ];
+
+// $headers = [
+//     'Content-Type' => 'application/json',
+//     'Authorization' => 'Bearer ' . $openaiApiKey,
+// ];
+
+// $client = new Client();
+// $response = $client->post($openaiEndpoint, [
+//     'headers' => $headers,
+//     'json' => $openaiData,
+// ]);
+
+// $result = json_decode($response->getBody(), true);
+// $chatGptAnalysis = $result['choices'][0]['text'];
+
+// echo 'ChatGPT Analysis: ' . $chatGptAnalysis . PHP_EOL;
--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function btnsContents(title,fun,id){
             </div>
             <div class="grid p-3" style="height: 50px;">
                 <div class="row">
-                    <a class="col-4 btn text-danger m-4" id="retourBtn" >
+                    <a class="col-4 btn text-danger m-4" id="retourBtn">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
                         </svg>
@@ -110,7 +110,9 @@ function btnsContents(title,fun,id){
                         onclick="(e)=>{
                             e.preventDefault();
                             
-                        }" class="col-4 btn btn-outline-success m-4" style="height: 40px;">suivant</button>
+                        }" class="col-4 btn btn-outline-success m-4" style="height: 40px;">
+                        suivant
+                    </button>
                 </div>
             </div>
         </form>
